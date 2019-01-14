7739a19aa39d95b8492d275992c34c00c447da00
Arthur <arthur.bazhanov@mifort.org>
2019-01-14 11:36:49 +0300

    flex 1

--- node-server-for-android/App.js
+++ node-server-for-android/App.js
@@ -13,7 +13,7 @@ export default class App extends React.Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 2,
+    flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
